8f4d240a7802e68de7409a60096520c6e3f942e2
jonathanlam1538 <jonathanlam1538@gmail.com>
2015-04-11 18:33:56 -0400

    routeConfig2

--- app/src/main/java/com/rocketstop/rocketstop/Stop.java
+++ app/src/main/java/com/rocketstop/rocketstop/Stop.java
@@ -1,8 +1,24 @@
 package com.rocketstop.rocketstop;
 
-/**
- * Created by DavidLam on 11/04/2015.
- */
+//The user's stopping destination.
+//consist of the stop tag number, title, lat, long, stop id.
 public class Stop
 {
+    public final String stopRouteNumber;
+    public final String stopRouteName;
+    public final String stopLat;
+    public final String stopLong;
+    public final String stopID;
+
+    //constructor
+    public Stop(String num, String name, String lat, String lon, String id)
+    {
+        stopRouteNumber = num;
+        stopRouteName = name;
+        stopLat = lat;
+        stopLong = lon;
+        stopID = id;
+    }
+
+
 }
