6a0a442d95db1e3d89a7f19144f10bf828822348
Jason Law <jasonkhlaw@gmail.com>
2015-07-01 18:00:25 -0400

    Initial SQLite TTC DB commit

---
+++ app/src/main/java/com/rocketstop/rocketstop/TTCDatabase.java
@@ -0,0 +1,31 @@
+package com.rocketstop.rocketstop;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+//Code adapted from: http://developer.android.com/training/basics/data-storage/databases.html
+
+public class TTCDatabase extends SQLiteOpenHelper{
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "TTCDatabase.db";
+
+    public TTCDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db){
+
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
